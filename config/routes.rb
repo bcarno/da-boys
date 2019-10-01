90d1e424462e95728bf4ac093a95714b47af0831
Chuck Coleman <ccoleman@etcnpt.com>
2019-10-01 11:55:29 -0400

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'guild/index'
 
+  devise_for :users
+
   resources :guildapps
 
   root 'guild#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "devise/registrations#new"
-  devise_for :users
+  #root to: "devise/registrations#new"
 end
